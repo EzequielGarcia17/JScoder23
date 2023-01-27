2f2c081b542cdede7a33eb83f3b45f14514ae76f
Ezequiel Garcia <ezequieladriangarcia@gmail.com>
2023-01-26 23:29:59 -0300

    entrega 1b

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let hola = alert ("Hola mi nombre es Robot, quisiera saber como esta conformada 
 
 let nombreFamiliar = prompt("Ingrese el nombre de su familiar. Para finalizar escriba Salir");
 
-while((nombreFamiliar !== "Salir" ) || (nombreFamiliar !== "salir" )){
+while(nombreFamiliar !== "Salir" ){
     parentescoFamiliar = prompt("Ingrese su parentesco con " + nombreFamiliar);
     edadFamiliar = parseInt(prompt("Ingresa la edad de " + nombreFamiliar ));
     console.log(nombreFamiliar + " es su " + parentescoFamiliar + " y tiene " + edadFamiliar + " años.");
