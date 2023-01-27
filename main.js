508b5c3c9d69253ee5148ea608a1ced7e25c6a11
Ezequiel Garcia <ezequieladriangarcia@gmail.com>
2023-01-26 23:38:54 -0300

    5

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 let hola = alert ("Hola mi nombre es Robot, quisiera saber como esta conformada tu familia. Sin malas intenciones :)");
 
-let nombreFamiliar = prompt("Ingrese el nombre de su familiar. Para finalizar escriba Salir");
+let nombreFamiliar = prompt("Ingrese el nombre de su familiar.");
 
 while(nombreFamiliar !== "Salir"){
     parentescoFamiliar = prompt("Ingrese su parentesco con " + nombreFamiliar);
     edadFamiliar = parseInt(prompt("Ingresa la edad de " + nombreFamiliar ));
     console.log(nombreFamiliar + " es su " + parentescoFamiliar + " y tiene " + edadFamiliar + " años.");
+    
     nombreFamiliar = prompt("Ingrese el nombre de otro familiar. Para finalizar escriba Salir");
 
 }
